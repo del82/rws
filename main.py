181e388825d335218e863af3ac35add0265ef144
David Lutz <del82@cornell.edu>
2014-09-27 22:59:00 -0400

    select one random tokenized sentence

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import random
 import webapp2
 import wikipedia
+import nltk.data
+#import jinja2
 from google.appengine.ext import ndb
+
+tokenizer = nltk.data.load('./english.pickle')
 
 class Article(ndb.Model):
     """ an article """
@@ -15,15 +20,13 @@ class MainHandler(webapp2.RequestHandler):
 
     def get(self):
         article = wikipedia.page(wikipedia.random(1))
-        local = Article(title       = article.title,
-                        content     = article.content,
-                        revision_id = article.revision_id)
+        local = Article(title         = article.title,
+                        content       = article.content,
+                        revision_id   = article.revision_id,
+                        sentence_list = tokenizer.tokenize(article.content))
         local_key = local.put()
-        self.response.write(article.content[:100])
-        # query = Article.query(Article.title == "Ishu Patel").fetch(1)[0]
-        # self.response.write(query.content[:100])
-
-
+        #self.response.write(article.summary)  # this exists.
+        self.response.write('<p>%s</p>' % random.choice([ i for i in local.sentence_list if not i.startswith('=')]))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
