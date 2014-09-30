9e0d23f399bb4220de6f29b4f953f9f167b465b2
David Lutz <del82@cornell.edu>
2014-09-30 16:30:27 -0400

    whatwhat

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-import os
+import os, json, codecs
 import random
 import webapp2
 import wikipedia
 import nltk.data
 import jinja2
 import logging
+
+# directly access the default handler and set its format directly
+#logging.getLogger().handlers[0].setFormatter(fr)
+
 logger = logging.getLogger(__name__)
+
 from itertools import takewhile
 
 from google.appengine.ext import ndb
@@ -32,31 +37,29 @@ class Article(ndb.Model):
 def _smart_tokenize(text):
 
     sentences = tokenizer.tokenize(text)
-    pred = lambda x: x != '== References =='
+    pred = lambda x: x != '== References ==' and x != '== External Links ==' and x != '== Bibliography =='
     return [s for s in list(takewhile(pred, sentences)) if not '==' in s]
 
 class MainHandler(webapp2.RequestHandler):
 
     def _safe_random_page(self):
-        logging.debug('Getting a safe random page')
+        logger.debug('Getting a safe random page')
         page_list = wikipedia.random(10)
         for title in page_list:
             try:
-                logging.debug('Trying %s' % title)
+                logger.debug('Trying %s' % title)
                 article = wikipedia.page(title)
                 return article
             except wikipedia.exceptions.DisambiguationError:
-                logging.info('Caught DisambiguationError')
+                logger.info('Caught DisambiguationError')
                 continue
         # if that doesn't work, barf.  In the future grab a cached page?
         raise wikipedia.exceptions.WikipediaException('too many disambiguation errors')
 
 
     def get(self):
-        #self.error(500)
-        #return None
         article = self._safe_random_page()
-        local = Article(title         = article.title,
+        local = Article(title         = unicode(article.title),
                         content       = article.content,
                         revision_id   = article.revision_id,
                         sentence_list = _smart_tokenize(article.content))
@@ -65,18 +68,28 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         #self.response.write(article.summary)  # this exists.
         random_sentence = random.choice(local.sentence_list)
-        self.response.write(template.render({'sentence' : random_sentence,
-                                             'title' : article.title,
+        self.response.write(template.render({'sentence' : unicode(random_sentence),
+                                             'title' : unicode(article.title),
                                              'revision_id' : article.revision_id}))
 
-class Wikipedia(object):
-    """ smart, cached access to wikipedia articles"""
 
-    def __init__(self):
-        pass
+class JSONHandler(MainHandler):
+    def get(self):
+        article = self._safe_random_page()
+        local = Article(title         = unicode(article.title),
+                        content       = article.content,
+                        revision_id   = article.revision_id,
+                        sentence_list = _smart_tokenize(article.content))
+        local_key = local.put()
+        random_sentence = random.choice(local.sentence_list)
+        response = {'sentence' : random_sentence,
+                    'link' : 'https://en.wikipedia.org/wiki/%s' % unicode(local.title)}
+        self.response.content_type = 'text/json'
+        self.response.write(json.dumps(response))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/api', JSONHandler)
 
 ], debug=True)
 
