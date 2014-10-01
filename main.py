6c7c6d57e5e4a70ad728c0609f20160c99fee31f
David Lutz <del82@cornell.edu>
2014-09-30 21:33:57 -0400

    add nicer 500 handling

--- main.py
+++ main.py
@@ -58,12 +58,24 @@ app = webapp2.WSGIApplication([
     ('/api', JSONHandler),
     ('/del82_admin', AdminHandler)
 
-], debug=True)
+], debug=False)
 
 def error_404(request, response, error):
     response.set_status(404)
     response.write(open('static/404.html').read())
 
 app.error_handlers[404] = error_404
-#app.error_handlers[500] = lambda x, response, z: response.write(open('static/500.html').read())
-#app.error_handlers[503] = lambda x, response, z: response.write(open('static/503.html').read())
+
+
+def error_500(request, response, error):
+    response.set_status(500)
+    response.write(open('static/500.html').read())
+
+app.error_handlers[500] = error_500
+
+
+def error_503(request, response, error):
+    response.set_status(503)
+    response.write(open('static/503.html').read())
+
+app.error_handlers[503] = error_503
