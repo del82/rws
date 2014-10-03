af074d028ecdaa4870d53871148faa9d56b92f0e
David Lutz <del82@cornell.edu>
2014-10-03 11:57:45 -0400

    add some basic exception logging; issue #5

--- main.py
+++ main.py
@@ -54,32 +54,36 @@ class AdminHandler(webapp2.RequestHandler):
 
 
 class FHHandler(webapp2.RequestHandler):
-    def get(self):
-        assert False
+    def get(self, error):
+        if not error: error = 500
+        webapp2.abort(error, detail="as requested.")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/api', JSONHandler),
     ('/del82_admin', AdminHandler),
-    ('/del82_admin/500', FHHandler)
+    ('/del82_admin/error/(\d+)?', FHHandler)
 
 ], debug=False)
 
-def error_404(request, response, error):
+def error_404(request, response, exception):
+    logger.exception(exception)
     response.set_status(404)
     response.write(open('static/404.html').read())
 
 app.error_handlers[404] = error_404
 
 
-def error_500(request, response, error):
+def error_500(request, response, exception):
+    logger.exception(exception)
     response.set_status(500)
     response.write(open('static/500.html').read())
 
 app.error_handlers[500] = error_500
 
 
-def error_503(request, response, error):
+def error_503(request, response, exception):
+    logger.exception(exception)
     response.set_status(503)
     response.write(open('static/503.html').read())
 
